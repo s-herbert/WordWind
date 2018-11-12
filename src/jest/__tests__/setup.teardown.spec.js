cb7c696a77489eaaed6865dc14f45f255aa77856
s-herbert <sth.accounts@gmail.com>
2018-11-11 21:22:14 -0800

    removed debug flag

--- src/jest/__tests__/setup.teardown.spec.js
+++ src/jest/__tests__/setup.teardown.spec.js
@@ -20,7 +20,7 @@ describe("setup unit tests", () => {
   });
 
   it("can spin up a connection", () => {
-    const memoryServer = new MongoMemoryServer({ autoStart: false ,debug:true});
+    const memoryServer = new MongoMemoryServer({ autoStart: false });
     expect.assertions(3);
     return setup
       .startMongo(memoryServer)

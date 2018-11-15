2ba2c36652d70b5e1c53e680c13b7a41a6258128
s-herbert <sth.accounts@gmail.com>
2018-11-14 19:20:01 -0800

    more tests, wrapped a lot of mongoose

--- src/db/__tests__/db.spec.js
+++ src/db/__tests__/db.spec.js
@@ -1,41 +1,47 @@
-const db = require("../db");
+
 
 describe("the persistent db", () => {
   describe("general functionality", () => {
-
+    const db = require("../db");
     it("knows if its connected",()=>{
       expect(db.isConnected()).toBe(false);
     });
-    
-    it("can connect", async () => {
+
+    it("shouldn't connect with bad uri",()=>{
+      expect.assertions(1);
+      return db.openConnection('mongodb://asldkfjsalffs.com', {connectTimeoutMS:4000})
+      .catch(err=>expect(err).toBeInstanceOf(Error));
+    },24000)
+
+    it("#openConnection() can connect", async () => {
+      expect.assertions(1)
       return db.openConnection()
       .then(()=>(expect(db.isConnected()).toBe(true)));
     });
 
-    it("can disconnect", async () => {
+    it("#closeConnection() can disconnect", async () => {
+      expect.assertions(1);
       await db.closeConnection();
       expect(db.isConnected()).toBe(false);
     });
   });
 
   describe("schema", () => {
-    // beforeAll(async () => {
-    //   await mongoose.connect(
-    //     global.__MONGO_URI__,
-    //     { dbName: global.__MONGO_DB_NAME__, useNewUrlParser: true }
-    //   );
-    // });
+    const db = require("../db");
+    beforeAll(async () => {
+      await db.openConnection();
+    });
 
-    // afterAll(async () => {
-    //   await mongoose.disconnect();
-    // });
+    afterAll(async () => {
+      await db.closeConnection();
+    });
 
-    xit("has a message model", () => {
-      expect(mongoose.connection.modelNames()).toContain("message");
+    it("has a message model", () => {
+      expect(db.getModelNames()).toContain("message");
     });
 
     xit("has a user model", () => {
-      expect(mongoose.connection.modelNames()).toContain("users");
+      expect(db.getModelNames()).toContain("users");
     });
   });
 });

4338cd497cb06b4c43f0a0e86f43dbf9615b298f
s-herbert <sth.accounts@gmail.com>
2018-11-16 23:51:59 -0800

    added tests for dbMap.getRequiredPaths, and for required paths of models

--- src/db/__tests__/dbMap.spec.js
+++ src/db/__tests__/dbMap.spec.js
@@ -2,55 +2,71 @@
 
 describe("the persistent db", () => {
   describe("general functionality", () => {
-    const dbCxn = require("../dbMap");
+    const dbMap = require("../dbMap");
     it("knows if its connected",()=>{
-      expect(dbCxn.isOpen()).toBe(false);
+      expect(dbMap.isOpen()).toBe(false);
     });
 
     it("#openConnection() can connect", async () => {
       expect.assertions(2)
-      expect(dbCxn.open).toBeInstanceOf(Function);
-      return dbCxn.open()
-      .then(()=>(expect(dbCxn.isOpen()).toBe(true)));
+      expect(dbMap.open).toBeInstanceOf(Function);
+      return dbMap.open()
+      .then(()=>(expect(dbMap.isOpen()).toBe(true)));
     });
 
     it("#getModelNames returns an array", () => {
-      expect(dbCxn.getModelNames).toBeInstanceOf(Function);
-      expect(dbCxn.getModelNames()).toBeInstanceOf(Array);
+      expect(dbMap.getModelNames).toBeInstanceOf(Function);
+      expect(dbMap.getModelNames()).toBeInstanceOf(Array);
+    });
+
+    it("#getRequiredPaths(model) returns an array", () => {
+      expect(dbMap.getModelNames).toBeInstanceOf(Function);
+      expect(dbMap.getRequiredPaths()).toBeInstanceOf(Array);
     });
 
     it("#closeConnection() can disconnect", async () => {
       expect.assertions(2);
-      expect(dbCxn.close).toBeInstanceOf(Function);
-      await dbCxn.close();
-      expect(dbCxn.isOpen()).toBe(false);
+      expect(dbMap.close).toBeInstanceOf(Function);
+      await dbMap.close();
+      expect(dbMap.isOpen()).toBe(false);
     });
 
     it("shouldn't connect with bad uri",()=>{
       expect.assertions(1);
-      return dbCxn.open('mongodb://asldkfjsalffs.com', {connectTimeoutMS:4000})
+      return dbMap.open('mongodb://asldkfjsalffs.com', {connectTimeoutMS:4000})
       .catch(err=>expect(err).toBeInstanceOf(Error));
     },24000)
+
   });
 
   describe("schema", () => {
-    const dbCxn = require("../dbMap");
+    const dbMap = require("../dbMap");
     beforeAll(async () => {
-      await dbCxn.open();
+      await dbMap.open();
     });
 
     afterAll(async () => {
-      await dbCxn.close();
+      await dbMap.close();
     });
 
     it("has a message model", () => {
       require('../models/Message')
-      expect(dbCxn.getModelNames()).toContain("Message");
+      expect(dbMap.getModelNames()).toContain("Message");
+    });
+
+    it("messages require an author", () => {
+      require('../models/Message')
+      expect(dbMap.getRequiredPaths('Message')).toContain("author");
     });
 
     it("has a user model", () => {
       require('../models/User')
-      expect(dbCxn.getModelNames()).toContain("User");
+      expect(dbMap.getModelNames()).toContain("User");
     });
+
+    it("users require a public key", () =>{
+      require('../models/User')
+      expect(dbMap.getRequiredPaths('User')).toContain("pub_key")
+    })
   });
 });

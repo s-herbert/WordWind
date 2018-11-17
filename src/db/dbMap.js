1fc81207f3a0fc7190a2acb1e0aa86140faea6c6
s-herbert <sth.accounts@gmail.com>
2018-11-16 22:21:38 -0800

    cleaned up

--- src/db/dbMap.js
+++ src/db/dbMap.js
@@ -1,8 +1,5 @@
 export const mongoose = require("mongoose");
 
-export const Schema = mongoose.Schema;
-export const model = mongoose.model;
-
 export async function open(URI = global.__MONGO_URI__, options = {}) {
   await mongoose.connect( URI, { ...options, useNewUrlParser: true});
   const db = mongoose.connection;
